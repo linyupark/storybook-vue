9bd8c8e842edff2bcc49235e7f7d19aa0f2953cd
lin-mini <lin@lin-minideiMac.local>
2019-07-30 19:24:01 +0800

    modal组件实装

---
+++ packages/resp/modal/index.vue
@@ -0,0 +1,292 @@
+<template>
+  <!-- 弹出层 -->
+  <div class="modal">
+    <div
+      class="modal-wrapper"
+      :class="{ visible: stateVisible }"
+    >
+      <div class="box">
+        <div
+          class="title"
+          v-html="title"
+        ></div>
+        <div class="content">
+          <!-- @slot 弹窗主体信息 -->
+          <slot />
+        </div>
+        <div
+          v-show="!$slots.buttons"
+          class="buttons"
+        >
+          <button
+            v-show="type === 'confirm'"
+            class="no"
+            @click="stateOnNo"
+          >
+            {{noText}}
+          </button>
+          <button
+            class="ok"
+            @click="stateOnOk"
+          >
+            {{this.okText}}
+          </button>
+        </div>
+        <div
+          v-show="$slots.buttons"
+          class="buttons"
+        >
+          <!-- @slot 自定义按钮处理 -->
+          <slot name="buttons" />
+        </div>
+      </div>
+
+    </div>
+    <div class="mask" :class="{ visible: stateVisible }"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .modal-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+    opacity: 0;
+    transition: all 0.3s;
+    &.visible {
+      z-index: 2;
+      opacity: 1;
+      .box {
+        opacity: 1;
+      }
+    }
+    .box {
+      width: px2vw(600);
+      background: #fff;
+      border-radius: px2vw(20);
+      overflow: hidden;
+      opacity: 0;
+      transition: all 0.3s;
+
+      .title {
+        text-align: center;
+        font-size: px2vw(32);
+        color: #272727;
+        font-weight: 600;
+        padding: px2vw(40);
+      }
+
+      .content {
+        padding: 0 px2vw(40) px2vw(40) px2vw(40);
+        color: #595959;
+        font-size: px2vw(28);
+        line-height: px2vw(40);
+      }
+      .buttons {
+        display: flex;
+        width: 100%;
+        height: px2vw(90);
+        border-top: 1px solid #f5f5f5;
+
+        .ok,
+        .no {
+          width: 100%;
+          text-align: center;
+          color: #3e86f7;
+          font-size: px2vw(36);
+          border: none;
+          background: #fff;
+          outline: none;
+        }
+
+        button:not(:first-child) {
+          border-left: 1px solid #f5f5f5;
+        }
+      }
+    }
+  }
+
+  .mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    z-index: -1;
+    opacity: 0;
+    background: rgba(#000, 0.1);
+    transition: all .3s;
+    &.visible {
+      z-index: 1;
+      opacity: 1;
+    }
+  }
+</style>
+<script>
+  import Vue from "vue";
+  import useTypeof from "@crh/vue/functions/useTypeof";
+  const RespModal = {
+    name: "RespModal",
+    components: {},
+    props: {
+      /** 弹窗类型 alert | confirm */
+      type: {
+        type: String,
+        default: "alert"
+      },
+      /** 弹窗标题 */
+      title: {
+        type: String,
+        default: "提示"
+      },
+      /** 取消文案 */
+      noText: {
+        type: String,
+        default: "取消"
+      },
+      /** 触发取消按钮对应处理 */
+      onNo: {
+        type: Function,
+        default: () => {}
+      },
+      /** 确定文案 */
+      okText: {
+        type: String,
+        default: "确定"
+      },
+      /** 触发确定按钮对应处理 */
+      onOk: {
+        type: Function,
+        default: () => {}
+      },
+      /** modal 信息自定义样式 */
+      inlineStyles: {
+        type: Object,
+        default: () => ({})
+      },
+      /** 默认是否展示 */
+      visible: {
+        type: Boolean,
+        default: false
+      },
+      /** 关闭时触发回调 */
+      onClose: {
+        type: Function,
+        default: () => {}
+      }
+    },
+    data() {
+      return {
+        /** 是否展示的内部状态 */
+        stateVisible: this.$props.visible,
+        stateOnNo: this.$props.onNo,
+        stateOnOk: this.$props.onOk,
+      };
+    },
+    methods: {
+      /**
+       * 执行显示操作
+       */
+      show() {
+        this.stateVisible = true;
+
+        // onOk onNo 函数则自动加入关闭操作
+        this.stateOnOk = async () => {
+          await this.onOk();
+          this.close();
+        };
+        this.stateOnNo = async () => {
+          await this.onNo();
+          this.close();
+        };
+      },
+      /**
+       * 执行关闭操作
+       */
+      close() {
+        this.stateVisible = false;
+
+        // 确保消失动画结束后再执行
+        setTimeout(() => {
+          this.onClose();
+          /**
+           * 发送close事件
+           * @type {Event}
+           */
+          this.$emit("close");
+        }, 300);
+      }
+    },
+    computed: {},
+    mounted() {
+      console.log("modal mounted");
+    }
+  };
+
+  /**
+   * 函数方式打开提示信息
+   */
+  RespModal.show = (props = {}) => {
+    // 还有展示中的 modal 则先执行close，确保只有一个有效的modal
+    if (RespModal._existComp) {
+      RespModal._existComp.close();
+    }
+
+    const ModalCompConstructor = Vue.extend(RespModal);
+
+    const ModalComp = new ModalCompConstructor({
+      // el: document.createElement('div'),
+      propsData: {
+        ...props
+      }
+    });
+
+    // 将消息塞入默认 slot
+    let contentNode = "";
+
+    // 如果传入的 content 为单纯文字
+    if (useTypeof(props.content) === "string") {
+      contentNode = ModalComp.$createElement("div", {
+        domProps: {
+          innerHTML: props.content
+        }
+      });
+    }
+
+    // 传入的 content 为 数组（多个vue组件或元素）
+    if (useTypeof(props.content) === "array") {
+      contentNode = ModalComp.$createElement("div", {}, props.content);
+    }
+
+    ModalComp.$slots.default = [contentNode];
+
+    // 当消息消失时候要销毁自动创建的dom节点避免页面冗余
+    ModalComp.$once("close", function() {
+      RespModal._existComp = null;
+      this.$destroy(true);
+      this.$el.parentNode.removeChild(this.$el);
+    });
+
+    // 模拟执行挂载
+    ModalComp.$mount();
+
+    // 插入dom
+    document.body.appendChild(ModalComp.$el);
+
+    // 默认直接展示信息（加延迟确保动画效果能生效）
+    setTimeout(() => {
+      ModalComp.show();
+    }, 100);
+
+    // 保持展示中组件暂存
+    RespModal._existComp = ModalComp;
+
+    return ModalComp;
+  };
+  export default RespModal;
+</script>
---
+++ packages/resp/modal/modal.stories.js
@@ -0,0 +1,75 @@
+import { storiesOf } from '@storybook/vue';
+import { text, select, boolean, number, array } from '@storybook/addon-knobs';
+
+import Modal from '@crh/vue/resp/modal/index';
+import IconSvg from '@crh/vue/icon/svg';
+
+storiesOf('反馈|Modal', module).add(
+  'Base',
+  () => ({
+    components: {
+      Modal,
+      IconSvg
+    },
+    props: {},
+    data() {
+      return {};
+    },
+    methods: {
+      openModal() {
+        Modal.show({
+          title: '警告',
+          content: '点确定后延迟1秒关闭',
+          onOk: async () => {
+            return await new Promise((resolve, rejects) => {
+              setTimeout(() => {
+                resolve('closeNow');
+              }, 1000);
+            });
+          }
+        });
+      },
+      openConfirmModal() {
+        Modal.show({
+          type: 'confirm',
+          title: '确认操作',
+          content: '确定要删除这个消息吗？',
+          onOk: () => {
+            alert('你点了确定');
+          }
+        });
+      },
+      openCustomModal() {
+        this.$refs.modal3.show();
+      },
+      select(v) {
+        alert(v);
+        this.$refs.modal3.close();
+      }
+    },
+    template: `
+      <div>
+        <h4>Modal.show (alert 提醒)</h4>
+        <center><button @click="openModal">警告</button></center>
+        <h4>Modal.show (confirm 确认)</h4>
+        <center><button @click="openConfirmModal">确认</button></center>
+        <h4>自定义按钮</h4>
+        <center><button @click="openCustomModal">自定义</button></center>
+        <Modal title="请选择" ref="modal3">
+          你喜欢哪个颜色？
+          <template slot="buttons">
+            <button class="ok" @click="select('红色')">红色</button>
+            <button class="ok" @click="select('黄色')">黄色</button>
+            <button class="ok" @click="select('绿色')">绿色</button>
+          </template>
+        </Modal>
+      </div>
+    `
+  }),
+  {
+    info: true,
+    knobs: {
+      escapeHTML: false
+    }
+  }
+);
