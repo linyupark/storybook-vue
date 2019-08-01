8d9d435dd8c73bb43279420ad041906067996b71
lin-mini <lin@lin-minideiMac.local>
2019-08-01 19:51:43 +0800

    优化函数

--- packages/functions/useProtocolBuffers.js
+++ packages/functions/useProtocolBuffers.js
@@ -1,86 +1,70 @@
 import useObjectQueryStringify from './useObjectQueryStringify';
 
 /**
- * 封装 protocol buffers 请求方法
- * 
- * 范例
- * .proto 转 js 命令npx pbjs -t json-module -w es6 -o src/?.js  src/?.proto
- * 
- * get请求
- * usePBRequest(protoRoot)('model.CommonRs')('get', '/api/testpb')
-  .then(resp => {
-    console.log(resp);
-  });
-  post 请求
-  usePBRequest(protoRoot, 'model.CommonReq')('model.CommonRs')(
-    'post',
-    '/api/posttestpb2',
-    {
-      userId: 1,
-      token: 'aaaa'
-    }
-  ).then(resp => {
-    console.log(resp);
-  });
- * @param {Object} protoRoot .proto文件转换成js的对象内容
- * @param {String} reqMessageName 对应请求消息格式的名称 比如 model.CommonReq
+ * 独立一个创建请求数据函数用于嵌套
+ * @param {Object} data 请求内容
+ * @param {Object} { protoRoot: proto 转 js 后的对象, reqType: 请求格式描述 } 
  */
-export default (protoRoot, reqMessageName) => {
-  let reqMessage;
-  if (reqMessageName) {
-    reqMessage = protoRoot.lookup(reqMessageName);
-  }
+export const createSendData = (data, {
+  protoRoot,
+  reqType
+}) => {
+  const reqMessage = protoRoot.lookup(reqType);
+  return reqMessage.encode(reqMessage.create(data)).finish();
+};
+
+/**
+ * 封装 protocol buffers 请求方法
+ * @param {Object} proto 转 js 后的对象
+ */
+export default protoRoot => ({ reqType, resType }) => {
+
+  /** reqType: 请求格式描述; resType: 响应格式描述 */
+  let resMessage;
+  resMessage = protoRoot.lookup(resType);
 
   /**
-   * @param {String} resMessageName 对应反馈消息格式的名称 比如 model.CommonRes
+   * @param {String} method 请求方法
+   * @param {String} url 请求地址
+   * @param {Object} data 请求内容
    */
-  return function(resMessageName) {
-    const resMessage = protoRoot.lookup(resMessageName);
+  return function(method = 'get', url, data = {}) {
+    let sendData;
+    if (method.toLowerCase() === 'post') {
+      sendData = createSendData(data, { protoRoot, reqType });
+    } else {
+      sendData = null;
+      const querySearch = useObjectQueryStringify(data);
+      url += querySearch === '' ? '' : `?${querySearch}`;
+    }
 
-    /**
-     * @param {String} method 请求方法
-     * @param {String} url 请求地址
-     * @param {Object} data 请求内容
-     */
-    return function(method = 'get', url, data = {}) {
-      let message, sendData;
-      if (method.toLowerCase() === 'post') {
-        message = reqMessage.create(data);
-        sendData = reqMessage.encode(message).finish();
-      } else {
-        sendData = null;
-        const querySearch = useObjectQueryStringify(data);
-        url += querySearch === '' ? '' : `?${querySearch}`;
-      }
-
-      return new Promise((rs, rj) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url, true);
-        xhr.responseType = 'arraybuffer';
-        xhr.setRequestHeader('Content-Type', 'application/x-protobuf');
-        xhr.onload = function(response) {
-          const result = resMessage.toObject(
-            resMessage.decode(new Uint8Array(response.target.response)),
-            {
-              // 确保 byte 格式的会转换为 base64string
-              bytes: String
-            }
-          );
-          // 循环结果，将 base64string => string，忽略解码错误
-          for (let k in result) {
-            if (~toString.call(result[k]).indexOf('String')) {
-              try {
-                result[k] = atob(result[k]);
-              } catch (e) {}
-            }
+    return new Promise((rs, rj) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url, true);
+      xhr.responseType = 'arraybuffer';
+      xhr.setRequestHeader('Content-Type', 'application/x-protobuf');
+      xhr.onload = function(response) {
+        const result = resMessage.toObject(
+          resMessage.decode(new Uint8Array(response.target.response)),
+          {
+            // 确保 byte 格式的会转换为 base64string
+            bytes: String
           }
-          rs(result);
-        };
-        xhr.onerror = function(error) {
-          rj(error);
-        };
-        xhr.send(sendData);
-      });
-    };
+        );
+        // 循环结果，将 base64string => string，忽略解码错误
+        for (let k in result) {
+          if (~toString.call(result[k]).indexOf('String')) {
+            try {
+              result[k] = atob(result[k]);
+            } catch (e) {}
+          }
+        }
+        rs(result);
+      };
+      xhr.onerror = function(error) {
+        rj(error);
+      };
+      xhr.send(sendData);
+    });
   };
 };
