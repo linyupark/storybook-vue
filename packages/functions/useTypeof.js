ff532090e0a93329adc5e56156a06f40803c6f28
lin-mini <lin@lin-minideiMac.local>
2019-08-15 15:53:40 +0800

    严谨

--- packages/functions/useTypeof.js
+++ packages/functions/useTypeof.js
@@ -2,7 +2,7 @@
  * 获取变量类型
  */
 export default function(mixed) {
-  const match = toString.call(mixed).match(/^\[object (\w+)\]$/);
+  const match = Object.prototype.toString.call(mixed).match(/^\[object (\w+)\]$/);
   if (!match) return 'unkonw';
   return match[1].toLowerCase();
 }
